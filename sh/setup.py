336fd413fda9ac26940cd5ecf2bfed792032b3b2
Joost Molenaar <j.j.molenaar@gmail.com>
2010-10-26 00:46:21 +0200

    ?

--- sh/setup.py
+++ sh/setup.py
@@ -3,10 +3,11 @@
 import distutils.core
 
 distutils.core.setup(
-	name='sh',
-	version='0.1',
-	description='Shell-like functions for Python',
-	author='Joost Molenaar',
-	author_email='j.j.molenaar@gmail.com',
-	url='http://github.com/JoostMolenaar/python',
-	packages=['sh'])
+    name='sh',
+    version='0.1',
+    description='Shell-like functions for Python',
+    author='Joost Molenaar',
+    author_email='j.j.molenaar@gmail.com',
+    url='http://github.com/JoostMolenaar/python',
+    py_modules=['sh'])
+	#packages=['sh'])
