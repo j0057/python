567433bb5963d0d916e30b9df26557a0389d7b59
Joost Molenaar <j.j.molenaar@gmail.com>
2010-10-25 21:12:45 +0200

    Added version 0.1 of sh package

---
+++ sh/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import distutils.core
+
+distutils.core.setup(
+	name='sh',
+	version='0.1',
+	description='Shell-like functions for Python',
+	author='Joost Molenaar',
+	author_email='j.j.molenaar@gmail.com',
+	url='http://github.com/JoostMolenaar/python',
+	packages=['sh'])
