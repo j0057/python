c40baa836d282931fe2d2bc57694c2f239f18bdf
Joost Molenaar <j.j.molenaar@gmail.com>
2010-11-08 23:54:41 +0100

    Redefined ffac

--- combinators/combinators.py
+++ combinators/combinators.py
@@ -12,15 +12,23 @@ Y = (lambda F:
 #S = lambda f: (lambda *a, **k: f(f, *a, **k))
 #Y = lambda F: (lambda g: g(g))(lambda f: F(lambda *a, **k: f(f)(*a, **k)))
 
-sqrange = lambda n: takewhile(lambda a: a * a <= n, count(1))
-limit   = lambda n, L: [v2 for (v1, v2) in izip(xrange(n), L)]
-factors = lambda n: (v for v in sqrange(n) if not n % v)
-ffac    = lambda n: limit(2, factors(n))[-1]
-pfac1   = S(lambda f, n:
-            (lambda i: [n] if i == 1 else f(f, i) + f(f, n / i))(i=ffac(n)))
-pfac2   = Y(lambda f: lambda n:
-            (lambda i: [n] if i == 1 else f(i) + f(n / i))(i=ffac(n)))
+def last(seq):
+    for item in seq:
+        pass
+    return item
 
+sqrange = lambda n: takewhile(lambda a: a * a <= n, count(1))
+factors = lambda n: (v for v in sqrange(n) if not n % v)
+take    = lambda n, seq: (lambda i: (i.next() for _ in xrange(n)))(iter(seq))
+ffac    = lambda n: last(take(2, factors(n)))
+
+pfac1   = S(lambda f, n:
+            (lambda i: [n] if i == 1 else f(f, i) + f(f, n / i))(ffac(n)))
+pfac2   = Y(lambda f: lambda n:
+            (lambda i: [n] if i == 1 else f(i) + f(n / i))(ffac(n)))
+pfac3   = Y(lambda f: lambda n:
+            (lambda i=ffac(n): [n] if i == 1 else f(i) + f(n / i))())
+            
 fact1   = S(lambda f, n: 
             1 if n == 1 else n * f(f, n - 1))
 fact2   = Y(lambda f: lambda n: 
@@ -34,7 +42,10 @@ fib2    = Y(lambda f: lambda n:
 if __name__ == '__main__':
     assert list(sqrange(10)) == [1, 2, 3]
     
-    assert limit(2, [1, 2, 3]) == [1, 2]
+    assert list(take(2, [])) == []
+    assert list(take(2, [1])) == [1]
+    assert list(take(2, [1, 2])) == [1, 2]
+    assert list(take(2, [1, 2, 3])) == [1, 2]
     
     assert list(factors(10)) == [1, 2]
     
@@ -46,8 +57,14 @@ if __name__ == '__main__':
     assert pfac2(10) == [2, 5]
     assert pfac2(20) == [2, 2, 5]
     
+    assert pfac3(160) == [2, 2, 2, 2, 2, 5]
+    assert pfac3(123456789012345) == [3, 5, 283, 3851, 7552031]
+    assert pfac3(123456789012) == [2, 2, 3, 10288065751]
+    
     assert fact1(4) == 24
     assert fact2(4) == 24
     
     assert fib1(4) == 5
+    assert fib2(4) == 5
+    
     
